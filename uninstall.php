07f1e2aaeca39303a1bef82f5aa78a47ae093fbd
Frank Bültge <frank@bueltge.de>
2012-09-20 15:52:07 +0200

    change path for uninstall plugin

--- uninstall.php
+++ uninstall.php
@@ -2,7 +2,7 @@
 if( ! defined( 'ABSPATH' ) && ! defined( 'WP_UNINSTALL_PLUGIN' ) )
 	exit();
 
-if ( is_multisite() && is_plugin_active_for_network( 'WP-Maintenance-Mode/wp-maintenance-mode.php' ) ) {
+if ( is_multisite() && is_plugin_active_for_network( 'wp-maintenance-mode/wp-maintenance-mode.php' ) ) {
 	add_site_option( 'wp-maintenance-mode' );
 	add_site_option( 'wp-maintenance-mode-msqd' );
 } else {
