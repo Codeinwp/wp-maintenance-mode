71dcd202a0bf77289fe7b745890d0a35a812768e
Frank Bültge <frank@bueltge.de>
2013-03-21 12:39:49 +0100

    change login url to better core function

--- site.php
+++ site.php
@@ -74,7 +74,7 @@
 					$adminloginmsg = '';
 				$adminloginstr = __( 'Admin-Logout', FB_WM_TEXTDOMAIN );
 			} else {
-				$adminlogin    = site_url('wp-login.php', 'login');
+				$adminlogin    = wp_login_url();
 				$adminloginmsg = '';
 				$adminloginstr = __( 'Admin-Login', FB_WM_TEXTDOMAIN );
 			}
