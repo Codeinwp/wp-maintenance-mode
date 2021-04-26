30063ad0f8edf6cc3a85f74ca3aa7eba1532efa3
George Jipa <contact@georgejipa.com>
2021-04-26 13:38:58 +0300

    add a better icon for GDPR tag

--- views/settings.php
+++ views/settings.php
@@ -38,7 +38,7 @@ defined( 'ABSPATH' ) || exit;
 				</a>
 				<a class="nav-tab" href="#gdpr">
 					<svg class="nav-tab-icon" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.618 5.984A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016zM12 9v2m0 4h.01" />
+						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
 					</svg>
 					<?php esc_html_e( 'GDPR', 'wp-maintenance-mode' ); ?>
 				</a>
