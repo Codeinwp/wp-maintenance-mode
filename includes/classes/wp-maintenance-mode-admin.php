9a389d50c8517d1d1da0a82ad1ec372383e773b5
arinaturcu <arinaturcu2@gmail.com>
2022-10-26 11:33:00 +0300

    chore: rename the new page

--- includes/classes/wp-maintenance-mode-admin.php
+++ includes/classes/wp-maintenance-mode-admin.php
@@ -647,7 +647,7 @@ if ( ! class_exists( 'WP_Maintenance_Mode_Admin' ) ) {
 				$post_arr['ID'] = $this->plugin_settings['design']['page_id'];
 				$page_id        = wp_update_post( $post_arr );
 			} else {
-				$post_arr['post_title'] = __( 'New Page', 'wp-maintenance-mode' );
+				$post_arr['post_title'] = __( 'Maintenance Page', 'wp-maintenance-mode' );
 				$page_id                = wp_insert_post( $post_arr );
 			}
 
